7eb1c314338e4cf4c40fac0a133f245c775ac404
Sebastien Maloron <contact@smaloron.eu>
2020-10-07 10:54:25 +0200

    Conception de l'application

---
+++ php/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Messagerie</title>
+</head>
+
+<body>
+
+    <h1>Ma super messagerie</h1>
+    <h3>Vous gardez toujours le contact avec vos amis</h3>
+
+    <form>
+        <h2>Nouveau message</h2>
+        <div>
+            <label>Auteur</label>
+            <input type="text" name="auteur">
+        </div>
+        <div>
+            <label>Message</label>
+            <input type="text" name="message">
+        </div>
+        <button type="submit">Valider</button>
+    </form>
+
+    <hr>
+
+    <h2>Les messages</h2>
+
+    <div>
+        <p>Le 10/10/2020 Ufuk a dit :</p>
+        <div>Bonjour à tous</div>
+    </div>
+
+    <div>
+        <p>Le 10/10/2020 Nazaré a dit :</p>
+        <div>Merci Ufuk</div>
+    </div>
+
+    <div>
+        <p>Le 10/10/2020 Sébastien a dit :</p>
+        <div>Au travail</div>
+    </div>
+
+</body>
+
+</html>
